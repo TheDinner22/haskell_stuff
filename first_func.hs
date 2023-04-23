c575d20389cc1811f07461e944a368bf9f90ff64
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-04-23 14:35:45 -0400

    functions and a comment

---
+++ first_func.hs
@@ -0,0 +1,7 @@
+tripleMe x = x+x+x
+
+threexy x y = tripleMe x + tripleMe y
+
+doubleIfSmall' x = (if x > 100 then x else x*2) + 1
+
+-- you are at the head command on the http://learnyouahaskell.com/starting-out
