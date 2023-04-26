80baa75f4403c57134ff3abd8d25e79a20aa45d6
joe goodman <jgood1133@gmail.com>
2023-04-26 16:42:10 -0400

    updated "bookmark"

--- first_func.hs
+++ first_func.hs
@@ -4,4 +4,4 @@ threexy x y = tripleMe x + tripleMe y
 
 doubleIfSmall' x = (if x > 100 then x else x*2) + 1
 
--- you are at the head command on the http://learnyouahaskell.com/starting-out
+-- you are at the "Success! Note that weeding out lists by predicates is also called filtering" on the http://learnyouahaskell.com/starting-out
