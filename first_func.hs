1f612187b30b2b3a57123d3e262bfe86ae8c1071
joe goodman <jgood1133@gmail.com>
2023-04-26 18:48:22 -0400

    boom beach list comprehension

--- first_func.hs
+++ first_func.hs
@@ -4,4 +4,8 @@ threexy x y = tripleMe x + tripleMe y
 
 doubleIfSmall' x = (if x > 100 then x else x*2) + 1
 
+-- where ls is a list of numbers
+oddBoomBeach ls = [if x > 10 then "BOOM" else "BEACH" | x <- ls, odd x]
+
 -- you are at the "Success! Note that weeding out lists by predicates is also called filtering" on the http://learnyouahaskell.com/starting-out
+-- now we at the first boom bang function (try and make fizzbuzz?)
