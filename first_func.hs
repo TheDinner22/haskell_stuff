8748f8c0416153a20ff2f92b6b4c672347da70f1
joe goodman <jgood1133@gmail.com>
2023-05-01 17:25:32 -0400

    comprehension inside itself

--- first_func.hs
+++ first_func.hs
@@ -11,4 +11,10 @@ foil xs ys = [x*y | x <- xs, y <- ys, x*y > 0 ]
 
 describe names props = [name ++ " " ++ prop | name <- names, prop <- props]
 
+length' xs = sum [1 | _ <- xs]
+
+removeNonJoeChar st = [c | c <- st, c `elem` ['j', 'o', 'e']]
+
+removeEven xxs = [ [ x | x <- xs, even x ] | xs <- xxs]
+
 -- you are at the "Success! Note that weeding out lists by predicates is also called filtering" on the http://learnyouahaskell.com/starting-out
