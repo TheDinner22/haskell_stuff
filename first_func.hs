024a2bd25c01800234efffe986085242d8eb9ddb
joe goodman <jgood1133@gmail.com>
2023-05-01 17:15:04 -0400

    more comprehensions

--- first_func.hs
+++ first_func.hs
@@ -7,5 +7,8 @@ doubleIfSmall' x = (if x > 100 then x else x*2) + 1
 -- where ls is a list of numbers
 oddBoomBeach ls = [if x > 10 then "BOOM" else "BEACH" | x <- ls, odd x]
 
+foil xs ys = [x*y | x <- xs, y <- ys, x*y > 0 ]
+
+describe names props = [name ++ " " ++ prop | name <- names, prop <- props]
+
 -- you are at the "Success! Note that weeding out lists by predicates is also called filtering" on the http://learnyouahaskell.com/starting-out
--- now we at the first boom bang function (try and make fizzbuzz?)
